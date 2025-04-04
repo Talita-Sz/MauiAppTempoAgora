06c642f6d67e10f1b9834834722566f5e74eb425
taica <taica@TALITA>
2025-04-04 18:07:12 -0300

    add mudanças.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,19 +1,20 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System.Diagnostics;
 
 namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        
-        private async void Button_Clicked(object sender, EventArgs e)
+
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -30,10 +31,21 @@ namespace MauiAppTempoAgora
                                          $"Nascer do Sol: {t.sunrise} \n" +
                                          $"Por do Sol: {t.sunset} \n" +
                                          $"Temp Máx: {t.temp_max} \n" +
-                                         $"Temp Min: {t.temp_min} \n";
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Visibilidade: {t.visibility} \n" +
+                                         $"Velocidade do vento: {t.speed} \n" +
+                                         $"Descrição: {t.description} \n";
 
                         lbl_res.Text = dados_previsao;
 
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                            $"type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind" +
+                            $"=default&zoom=3&overlay=wind&product=ecmwf&level=surface&" +
+                            $"lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
                     }
                     else
                     {
@@ -53,6 +65,71 @@ namespace MauiAppTempoAgora
             }
 
         }
-    }
 
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request =
+                    new GeolocationRequest(GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10));
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                        $"Longitude{local.Longitude} \n";
+
+                    lbl_coords.Text = local_disp;
+
+                    GetCidade(local.Latitude, local.Longitude);//pega o nome da cidade q ta nas coord
+                }
+                else
+                {
+                    lbl_coords.Text = "Nenhuma Localização";
+                }
+
+
+
+            }
+            catch (FeatureNotSupportedException fnEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não suporta", fnEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            }
+            catch (Exception ex) 
+            { 
+               await DisplayAlert("Erro: Obtenção do nome da cidade", ex.Message, "OK");
+            }
+
+        }
+
+    }
 }
